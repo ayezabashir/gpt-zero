224d78097ae671711dfc5cb52abe3cc965b24469
Ayeza <ayezabashir442@gmail.com>
2024-01-10 00:21:35 +0500

    presettings

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+const Header = () => {
+    return (
+        <>
+            <div className="header p-2">
+                header
+            </div>
+        </>
+    )
+}
+
+export default Header
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: [
+    "./src/components/header/Header.jsx",
+  ],
   theme: {
+    spacing: {
+      0: '0px',
+      1: '4px',
+      2: '6px',
+      3: '8px',
+      4: '10px',
+      5: '12px',
+      6: '14px'
+    },
     extend: {},
   },
   plugins: [],
