610ebbaca189a6d4e94785cc123e3524f95b136e
Ayeza <ayezabashir442@gmail.com>
2024-01-10 00:28:09 +0500

    footer

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,16 @@
+const Footer = () => {
+    return (
+        <div className="footer">
+            <ul>
+                <li>&copright; 2023-2024 GPTZero</li>
+                <li>Contact Us</li>
+                <li>Support</li>
+                <li>News</li>
+                <li>Affiliates</li>
+                <li>Cookies</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Footer
