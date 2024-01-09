d7efb16b760d6b80e6a9f66243c361cc0d45bb81
Ayeza <ayezabashir442@gmail.com>
2024-01-10 00:26:03 +0500

    header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,7 +2,23 @@ const Header = () => {
     return (
         <>
             <div className="header p-2">
-                header
+                <div className="img">
+                    <img src="/logo.svg" alt="" />
+                    GPTZero
+                </div>
+                <div className="header-links">
+                    <ul>
+                        <li>solutions</li>
+                        <li>resources</li>
+                        <li>pricing</li>
+                        <li>team</li>
+                        <li>login</li>
+                    </ul>
+                </div>
+                <div className="buttons">
+                    <button>Sales</button>
+                    <button>get started</button>
+                </div>
             </div>
         </>
     )
