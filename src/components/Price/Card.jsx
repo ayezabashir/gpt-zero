9e4bdc0e642c9347adbc7c39cf9a1c9540e573b4
Ayeza <ayezabashir442@gmail.com>
2024-01-26 23:33:16 +0500

    card

--- src/components/Price/Card.jsx
+++ src/components/Price/Card.jsx
@@ -1,12 +1,14 @@
+/* eslint-disable react/prop-types */
 const Card = ({ pic, title, heading, para, color }) => {
     return (
-        <div className="">
-            <img className="w-100" src={pic} alt="" />
+        <div className="p-6 shadow-lg max-w-[30%]">
+            <img className="w-100 mb-5" src={pic} alt="" />
             <div className="card-body">
-                <h2 className={`${color} uppercase font-medium`}>{title}</h2>
-                <h3 className="">{heading}</h3>
-                <p className="card-text">{para}</p>
+                <h2 className={`${color} uppercase font-bold text-sm`}>{title}</h2>
+                <h3 className="text-txt-black text-2xl font-bold my-3">{heading}</h3>
+                <p className="card-text font-light text-lg">{para}</p>
             </div>
+            <button className="border rounded-md mt-4 px-4 py-3 self-end">Learn More</button>
         </div>
     )
 }
