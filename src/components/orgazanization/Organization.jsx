66f3e639c0c3e88ae7075733c71c28c0f780b3e4
Ayeza <ayezabashir442@gmail.com>
2024-01-27 20:48:31 +0500

    organization

--- src/components/orgazanization/Organization.jsx
+++ src/components/orgazanization/Organization.jsx
@@ -3,42 +3,18 @@ const Organization = () => {
         <div className="container my-10 flex flex-col items-center justify-center">
             <h2 className="font-bold text-sm my-10 text-txt-black">AI TEXT DETECTION AND ANALYSIS TRUSTED BY LEADING ORGANIZATIONS</h2>
             <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 items-center lg:container">
-                <div>
-                    <img loading="lazy" className="m-3" src="/anthology.svg" alt="" />
-                </div>
-                <div>
-                    <img loading="lazy" className="m-3" src="/berkley.svg" alt="" />
-                </div>
-                <div>
-                    <img loading="lazy" className="m-3" src="/canvas.svg" alt="" />
-                </div>
-                <div>
-                    <img loading="lazy" className="m-3" src="/coursify.svg" alt="" />
-                </div>
-                <div>
-                    <img loading="lazy" className="m-3" src="/duke.svg" alt="" />
-                </div>
-                <div>
-                    <img loading="lazy" className="m-3" src="/isb.svg" alt="" />
-                </div>
-                <div>
-                    <img loading="lazy" className="m-3" src="/k16.svg" alt="" />
-                </div>
-                <div>
-                    <img loading="lazy" className="m-3" src="/newsGuard.svg" alt="" />
-                </div>
-                <div>
-                    <img loading="lazy" className="m-3" src="/nextCloud.svg" alt="" />
-                </div>
-                <div>
-                    <img loading="lazy" className="m-3" src="/nus.svg" alt="" />
-                </div>
-                <div>
-                    <img loading="lazy" className="m-3" src="/nyu.svg" alt="" />
-                </div>
-                <div>
-                    <img loading="lazy" className="m-3" src="/translate.svg" alt="" />
-                </div>
+                <img loading="lazy" className="m-3" src="/anthology.svg" alt="" />
+                <img loading="lazy" className="m-3" src="/berkley.svg" alt="" />
+                <img loading="lazy" className="m-3" src="/canvas.svg" alt="" />
+                <img loading="lazy" className="m-3" src="/coursify.svg" alt="" />
+                <img loading="lazy" className="m-3" src="/duke.svg" alt="" />
+                <img loading="lazy" className="m-3" src="/isb.svg" alt="" />
+                <img loading="lazy" className="m-3" src="/k16.svg" alt="" />
+                <img loading="lazy" className="m-3" src="/newsGuard.svg" alt="" />
+                <img loading="lazy" className="m-3" src="/nextCloud.svg" alt="" />
+                <img loading="lazy" className="m-3" src="/nus.svg" alt="" />
+                <img loading="lazy" className="m-3" src="/nyu.svg" alt="" />
+                <img loading="lazy" className="m-3" src="/translate.svg" alt="" />
             </div>
         </div>
     )
