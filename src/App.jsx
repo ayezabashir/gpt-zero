68f9fbc854df6ac1c654cf0e33610d75f6c7a41b
Ayeza <ayezabashir442@gmail.com>
2024-02-06 23:24:37 +0500

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import Footer from "./components/footer/Footer"
 import Header from "./components/header/Header"
-import Home from "../src/pages/Home"
-
+import Prices from "./components/Price/Prices"
+import Discover from "./components/discover/Discover"
+import Faqs from "./components/faq/Faqs"
+import Hero from "./components/hero/Hero"
+import Leading from "./components/leadingResearch/Leading"
+import NewsExclusive from "./components/newsexclusve/NewsExclusive"
+import Organization from "./components/orgazanization/Organization"
+import Plans from "./components/plans/Plans"
 function App() {
 
   return (
@@ -9,7 +15,14 @@ function App() {
       <Header />
       <img src="/bg.svg" alt="background image"
         className="absolute right-0 top-0 z-0 w-[100vw]" />
-      <Home />
+      <Hero />
+      <Discover />
+      <NewsExclusive />
+      <Leading />
+      <Prices />
+      <Organization />
+      <Plans />
+      <Faqs />
       <Footer />
     </>
   )
