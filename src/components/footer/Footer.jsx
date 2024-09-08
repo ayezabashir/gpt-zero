8229806f9cfd8a68771e5796856cf0b98dd4a668
Ayeza <ayezabashir442@gmail.com>
2024-09-08 15:56:50 +0500

    footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,24 +4,105 @@ import { FaFacebook } from "react-icons/fa6";
 const Footer = () => {
   return (
     <div className="relative bottom-0 my-10 container">
-      <ul className="text-txt-black flex justify-evenly items-center flex-wrap">
-        <li>© 2023-2024 GPTZero |</li>
-        <li className="underline">Contact Us |</li>
-        <li className="underline">Support |</li>
-        <li className="underline">News |</li>
-        <li className="underline">Affiliates |</li>
-        <li className="underline">Cookie Policy |</li>
-        <li className="underline">Privacy Policy |</li>
-        <li className="underline">Terms of Use |</li>
-        <li>Powered by AWS </li>
-      </ul>
-      <div className="text-txt-black mt-5 flex justify-center gap-2 items-center">
-        Follow us on :
-        <div className="flex gap-3 items-center">
-          <IoLogoTwitter className="text-2xl transition ease-in-out delay-150 hover:scale-110 duration-300 cursor-pointer" />
-          <FaFacebook className="text-xl transition ease-in-out delay-150 hover:scale-110 duration-300 cursor-pointer" />
-          <IoLogoLinkedin className="text-2xl transition ease-in-out delay-150 hover:scale-110 duration-300 cursor-pointer" />
-        </div>
+      <div className="hover:scale-110 transition delay-150 hover:translate-y-1 duration-300 ease-in-out cursor-pointer flex items-center">
+        <img className="w-5" src="/logo.svg" alt="logo" />
+        <h2 className="text-txt-black text-base md:text-lg ml-2 hover:underline font-bold my-5">
+          GPTZero
+        </h2>
+      </div>
+      <div className="grid grid-cols-2 md:grid-cols-3 gap-5">
+        <ul className="text-txt-black">
+          <li className="font-medium text-txt-black my-3 text-base">
+            Products
+          </li>
+          <li className="hover:underline font-regular text-sm my-1">Origin</li>
+          <li className="hover:underline font-regular text-sm my-1">
+            Writing Report
+          </li>
+          <li className="hover:underline font-regular text-sm my-1">API</li>
+          <li className="hover:underline font-regular text-sm my-1">
+            Canvas Integration
+          </li>
+          <li className="hover:underline font-regular text-sm my-1">
+            Microsoft Word Add-on
+          </li>
+          <li className="hover:underline font-regular text-sm my-1">
+            Plagiarism Checker
+          </li>
+        </ul>
+        <ul className="text-txt-black ">
+          <li className="font-medium text-txt-black my-3 text-base">
+            Resource
+          </li>
+          <li className="hover:underline font-regular text-sm my-1">Pricing</li>
+          <li className="hover:underline font-regular text-sm my-1">Sales</li>
+          <li className="hover:underline font-regular text-sm my-1">
+            Technology
+          </li>
+          <li className="hover:underline font-regular text-sm my-1">
+            Education
+          </li>
+          <li className="hover:underline font-regular text-sm my-1">
+            Teachers Guide
+          </li>
+          <li className="hover:underline font-regular text-sm my-1">
+            AI Guide for Seniors
+          </li>
+        </ul>
+        <ul className="text-txt-black ">
+          <li className="font-medium text-txt-black my-3 text-base">Company</li>
+          <li className="hover:underline font-regular text-sm my-1">
+            About us
+          </li>
+          <li className="hover:underline font-regular text-sm my-1">Team</li>
+          <li className="hover:underline font-regular text-sm my-1">
+            Affiliates
+          </li>
+          <li className="hover:underline font-regular text-sm my-1">Press</li>
+          <li className="hover:underline font-regular text-sm my-1">Blog</li>
+        </ul>
+        <ul className="text-txt-black ">
+          <li className="font-medium text-txt-black my-3 text-base">Help</li>
+          <li className="hover:underline font-regular text-sm my-1">FAQ</li>
+          <li className="hover:underline font-regular text-sm my-1">Help</li>
+          <li className="hover:underline font-regular text-sm my-1">
+            Privacy Policy
+          </li>
+          <li className="hover:underline font-regular text-sm my-1">
+            Cookie Policy
+          </li>
+          <li className="hover:underline font-regular text-sm my-1">
+            Terms of Use
+          </li>
+        </ul>
+        <ul className="text-txt-black ">
+          <li className="font-medium text-txt-black my-3 text-base">Contact</li>
+          <li className="hover:underline font-regular text-sm my-1">
+            team@gptzero.me
+          </li>
+        </ul>
+        <ul className="text-txt-black col-span-full md:col-span-1">
+          <li className="font-medium text-txt-black my-3 text-base">
+            Stay up to date
+          </li>
+          <li>
+            <input
+              placeholder="Enter your email"
+              type="email"
+              name="email"
+              className="px-3 py-4 w-full focus:border border-zinc-200 outline-none rounded-sm"
+            />
+            <button className="bg-[#3B3A3B] my-3 text-white w-[200px] py-4 font-light text-sm rounded-md">
+              Subscribe
+            </button>
+          </li>
+          <li className="flex gap-3 items-center">
+            <IoLogoTwitter className="text-2xl transition ease-in-out delay-150 hover:scale-110 duration-300 cursor-pointer" />
+            <FaFacebook className="text-xl transition ease-in-out delay-150 hover:scale-110 duration-300 cursor-pointer" />
+            <IoLogoLinkedin className="text-2xl transition ease-in-out delay-150 hover:scale-110 duration-300 cursor-pointer" />
+          </li>
+          <li className=" font-regular text-sm my-1">© 2023 - 2024 GPTZero</li>
+        </ul>
       </div>
     </div>
   );
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,76 +2,127 @@ import { useState } from "react";
 import { IoIosArrowUp } from "react-icons/io";
 import { IoIosArrowDown } from "react-icons/io";
 import NavItems from "./NavItems";
-import { solutions, products, resources, support } from './../../assets/data/data'
+import {
+  solutions,
+  products,
+  resources,
+  support,
+} from "./../../assets/data/data";
 import MenuItems from "./MenuItems";
 
 const Header = () => {
-    const [dropdown, setDropdown] = useState(true);
-    const [dropdown2, setDropdown2] = useState(true);
-    const [menu, setMenu] = useState(true);
-    const handleDropdown = () => {
-        setDropdown(!dropdown);
-        setDropdown2(true);
-    }
-    const handleDropdown2 = () => {
-        setDropdown2(!dropdown2);
-        setDropdown(true);
-    }
+  const [dropdown, setDropdown] = useState(true);
+  const [dropdown2, setDropdown2] = useState(true);
+  const [menu, setMenu] = useState(true);
+  const handleDropdown = () => {
+    setDropdown(!dropdown);
+    setDropdown2(true);
+  };
+  const handleDropdown2 = () => {
+    setDropdown2(!dropdown2);
+    setDropdown(true);
+  };
 
-    const showMenu = () => {
-        setMenu(!menu);
-    }
+  const showMenu = () => {
+    setMenu(!menu);
+  };
 
-    return (
-        <div className="header bg-transparent z-10 relative">
-            <div className="nav-bar flex justify-between items-center py-3 px-8">
-                <div className="nav-bar-items flex items-center justify-center img">
-                    <div className="hover:scale-110 transition delay-150 hover:translate-y-1 duration-300 ease-in-out cursor-pointer flex items-center justify-between mr-8">
-                        <img src="/logo.svg" alt="" />
-                        <h1 className="text-txt-black text-xl md:text-2xl ml-2 font-bold">GPTZero</h1>
-                    </div>
-                    <ul className="hidden lg:flex navbar-items-item items-center justify-center space-x-1">
-                        <li onClick={handleDropdown} className="item tracking-widest hover:scale-110 transition hover:bg-[#0000001a] delay-150 hover:translate-y-0.25 duration-300 ease-in-out cursor-pointer  flex items-center px-4 py-3 rounded-md text-txt-black uppercase text-sm font-medium ">
-                            solutions
-                            {
-                                dropdown ? <IoIosArrowUp className="ml-1" /> : <IoIosArrowDown className="ml-1" />
-                            }
-                        </li>
-                        <li onClick={handleDropdown2} className="item hover:scale-110 transition delay-150 hover:translate-y-0.25 duration-300 ease-in-out cursor-pointer  flex items-center px-4 py-3 rounded-md text-txt-black uppercase text-sm font-medium">
-                            resources
-                            {
-                                dropdown2 ? <IoIosArrowUp className="ml-1" /> : <IoIosArrowDown className="ml-1" />
-                            }
-                        </li>
-                        <li className="item hover:scale-110 transition delay-150 hover:translate-y-0.25 duration-300 ease-in-out cursor-pointer  px-4 py-3 rounded-md text-txt-black uppercase text-sm font-medium">pricing</li>
-                        <li className="item hover:scale-110 transition delay-150 hover:translate-y-0.25 duration-300 ease-in-out cursor-pointer  px-4 py-3 rounded-md text-txt-black uppercase text-sm font-medium">team</li>
-                    </ul>
-                </div>
-
-                <div className="buttons flex items-center justify-center space-x-2 md:space-x-5">
-                    <button className="hidden md:block px-4 py-3 text-txt-black uppercase text-sm font-medium hover:scale-110 transition delay-150 hover:translate-y-0.25 duration-300 ease-in-out cursor-pointer">log in</button>
-                    <button className="hidden lg:block text-xs md:text-sm px-4 py-3 rounded-md border border-txt-black hover:bg-btn-hover-black hover:transition-all hover:text-white uppercase font-light">Sales</button>
-                    <button className="px-4 py-3 text-xs md:text-sm rounded-md bg-btn-black md:order-2 md:border-btn-black hover:bg-btn-hover-black hover:border-btn-hover-black text-white uppercase font-light">get started</button>
-                    <div className="lg:hidden cursor-pointer" onClick={showMenu}>
-                        <div className={`bg-txt-black w-6 h-1 ${menu ? '' : 'rotate-45 -mb-1 transition duration-500 '}`}></div>
-                        <div className={`bg-txt-black w-6 mt-1 mb-1 h-1 ${menu ? 'block' : 'hidden'}`}></div>
-                        <div className={`bg-txt-black w-6 mb-1 h-1 ${menu ? '' : '-rotate-45 transition duration-500 '}`}></div>
-                    </div>
-                </div>
-            </div>
-            <div className="hidden lg:block">
-                {
-                    dropdown ? '' : <NavItems navItemOne='Solutions' itemsOne={solutions} navItemTwo='Products' itemsTwo={products} />
-                }
-                {
-                    dropdown2 ? '' : <NavItems navItemOne='Resources' itemsOne={resources} navItemTwo='support' itemsTwo={support} />
-                }
-            </div>
-            {
-                menu ? '' : <MenuItems />
-            }
+  return (
+    <div className="header bg-transparent z-10 relative">
+      <div className="nav-bar flex justify-between items-center py-3 px-8">
+        <div className="nav-bar-items flex items-center justify-center img">
+          <div className="hover:scale-110 transition delay-150 hover:translate-y-1 duration-300 ease-in-out cursor-pointer flex items-center justify-between mr-8">
+            <img src="/logo.svg" alt="logo" />
+            <h1 className="text-txt-black text-xl md:text-2xl ml-2 font-bold">
+              GPTZero
+            </h1>
+          </div>
+          <ul className="hidden lg:flex navbar-items-item items-center justify-center space-x-1">
+            <li
+              onClick={handleDropdown}
+              className="item tracking-widest hover:scale-110 transition hover:bg-[#0000001a] delay-150 hover:translate-y-0.25 duration-300 ease-in-out cursor-pointer  flex items-center px-4 py-3 rounded-md text-txt-black uppercase text-sm font-medium "
+            >
+              solutions
+              {dropdown ? (
+                <IoIosArrowUp className="ml-1" />
+              ) : (
+                <IoIosArrowDown className="ml-1" />
+              )}
+            </li>
+            <li
+              onClick={handleDropdown2}
+              className="item hover:scale-110 transition delay-150 hover:translate-y-0.25 duration-300 ease-in-out cursor-pointer  flex items-center px-4 py-3 rounded-md text-txt-black uppercase text-sm font-medium"
+            >
+              resources
+              {dropdown2 ? (
+                <IoIosArrowUp className="ml-1" />
+              ) : (
+                <IoIosArrowDown className="ml-1" />
+              )}
+            </li>
+            <li className="item hover:scale-110 transition delay-150 hover:translate-y-0.25 duration-300 ease-in-out cursor-pointer  px-4 py-3 rounded-md text-txt-black uppercase text-sm font-medium">
+              pricing
+            </li>
+            <li className="item hover:scale-110 transition delay-150 hover:translate-y-0.25 duration-300 ease-in-out cursor-pointer  px-4 py-3 rounded-md text-txt-black uppercase text-sm font-medium">
+              team
+            </li>
+          </ul>
         </div>
-    )
-}
 
-export default Header
+        <div className="buttons flex items-center justify-center space-x-2 md:space-x-5">
+          <button className="hidden md:block px-4 py-3 text-txt-black uppercase text-sm font-medium hover:scale-110 transition delay-150 hover:translate-y-0.25 duration-300 ease-in-out cursor-pointer">
+            log in
+          </button>
+          <button className="hidden lg:block text-xs md:text-sm px-4 py-3 rounded-md border border-txt-black hover:bg-btn-hover-black hover:transition-all hover:text-white uppercase font-light">
+            Sales
+          </button>
+          <button className="px-4 py-3 text-xs md:text-sm rounded-md bg-btn-black md:order-2 md:border-btn-black hover:bg-btn-hover-black hover:border-btn-hover-black text-white uppercase font-light">
+            get started
+          </button>
+          <div className="lg:hidden cursor-pointer" onClick={showMenu}>
+            <div
+              className={`bg-txt-black w-6 h-1 ${
+                menu ? "" : "rotate-45 -mb-1 transition duration-500 "
+              }`}
+            ></div>
+            <div
+              className={`bg-txt-black w-6 mt-1 mb-1 h-1 ${
+                menu ? "block" : "hidden"
+              }`}
+            ></div>
+            <div
+              className={`bg-txt-black w-6 mb-1 h-1 ${
+                menu ? "" : "-rotate-45 transition duration-500 "
+              }`}
+            ></div>
+          </div>
+        </div>
+      </div>
+      <div className="hidden lg:block">
+        {dropdown ? (
+          ""
+        ) : (
+          <NavItems
+            navItemOne="Solutions"
+            itemsOne={solutions}
+            navItemTwo="Products"
+            itemsTwo={products}
+          />
+        )}
+        {dropdown2 ? (
+          ""
+        ) : (
+          <NavItems
+            navItemOne="Resources"
+            itemsOne={resources}
+            navItemTwo="support"
+            itemsTwo={support}
+          />
+        )}
+      </div>
+      {menu ? "" : <MenuItems />}
+    </div>
+  );
+};
+
+export default Header;
