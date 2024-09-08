952eada677290748739aadfccc4dd80a7e3f4ff9
Ayeza <ayezabashir442@gmail.com>
2024-09-08 16:10:40 +0500

    fixed styles

--- src/components/discover/Discover.jsx
+++ src/components/discover/Discover.jsx
@@ -5,7 +5,7 @@ const Discover = () => {
     <div className="lg:w-[85%] mx-auto shadow-md rounded-sm bg-white z-50 opacity-100 relative my-10 pb-10 flex flex-col ">
       <div className="lg:w-[60%]">
         <div className="container md:mt-11 md:mr-2">
-          <h2 className="text-blue font-bold md:text-base text-sm">
+          <h2 className="text-blue font-bold md:text-base text-sm pt-5">
             WITH MILLIONS OF USERS
           </h2>
           <h3 className="text-txt-black mt-3 mb-5 md:text-3xl text-2xl font-bold">
--- src/components/faq/Faqs.jsx
+++ src/components/faq/Faqs.jsx
@@ -1,18 +1,24 @@
-import Question from "./Question"
+import Question from "./Question";
 const Faqs = () => {
-    return (
-        <div className="container my-10 lg:flex">
-            <div className="lg:w-1/3 lg:mx-10">
-                <h2 className="font-bold text-xl text-txt-black">General FAQs about our AI Detector</h2>
-                <p className="text-txt-black mt-5">
-                    Everything you need to know about GPTZero and our chat gpt detector. Can’t find an answer? You can talk to our <span className="underline cursor-pointer">customer service team.</span>
-                </p>
-            </div>
-            <div className="lg:w-2/3 my-5 lg:my-0 lg:mx-10">
-                <Question />
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="container my-10 lg:flex">
+      <div className="lg:w-1/3 lg:mx-10">
+        <h2 className="font-bold md:text-xl text-lg text-txt-black">
+          General FAQs about our AI Detector
+        </h2>
+        <p className="text-txt-black md:text-base text-sm mt-5">
+          Everything you need to know about GPTZero and our chat gpt detector.
+          Can’t find an answer? You can talk to our{" "}
+          <span className="underline cursor-pointer">
+            customer service team.
+          </span>
+        </p>
+      </div>
+      <div className="lg:w-2/3 my-5 lg:my-0 lg:mx-10">
+        <Question />
+      </div>
+    </div>
+  );
+};
 
-export default Faqs
+export default Faqs;
